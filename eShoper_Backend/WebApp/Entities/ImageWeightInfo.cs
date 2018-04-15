5add2dcde3a3f805161801413fff5ab6a9e7a925
Charles Rhamazany <crhamazany@outlook.com>
2018-04-14 23:58:09 -0400

    images for feature items added

---
+++ eShoper_Backend/WebApp/Entities/ImageWeightInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Entities
+{
+    public class ImageWeightInfo
+    {
+        public int Id { get; set; }
+        public PageLocation PageLocation { get; set; }
+        public int SizeOrder { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Resolution { get; set; }
+    }
+}
---
+++ eShoper_Backend/WebApp/Services/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using WebApp.Entities;
+using WebApp.Interfaces;
+using WebApp.Models.ProductViewModels;
+
+namespace WebApp.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IEShoperUnit _unit;
+
+        public ProductService(IEShoperUnit unit)
+        {
+            _unit = unit;
+        }
+
+        public IEnumerable<ProductDto> GetProductDtosForFeatureItems()
+        {
+            return GetProductDtosByLocation(PageLocation.Home_Feature_Items);
+        }
+
+        public IEnumerable<ProductDto> GetProductDtosForHomeSlider()
+        {
+            return GetProductDtosByLocation(PageLocation.Home_Slider);
+        }
+
+        private IEnumerable<ProductDto> GetProductDtosByLocation(PageLocation pageLocation)
+        {
+            var sliderItems = _unit.Products.GetProductsByPageLocation(pageLocation);
+            var productDtos = Mapper.Map<IEnumerable<ProductDto>>(sliderItems);
+            return productDtos;
+        }
+    }
+}
