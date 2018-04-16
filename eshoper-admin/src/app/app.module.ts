bef1bd687653f2fc342c1558c3502e025de521fc
Charles Rhamazany <crhamazany@outlook.com>
2018-04-15 23:42:39 -0400

    few components added - navigation works

--- eshoper-admin/src/app/app.module.ts
+++ eshoper-admin/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaintenanceModule } from './maintenance/maintenance.module';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot([
+      { path: '', component: DashboardComponent }   
+    ]),
+    MaintenanceModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ eshoper-admin/src/app/maintenance/maintenance.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { ProductListComponent } from './product-list/product-list.component';
+import { BrandListComponent } from './brand-list/brand-list.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { ImageListComponent } from './image-list/image-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([      
+      { path: 'brands', component: BrandListComponent },
+      { path: 'categories', component: CategoryListComponent },
+      { path: 'images', component: BrandListComponent },
+      { path: 'products', component: ProductListComponent }
+    ])
+  ],
+  declarations: [
+    ProductListComponent, 
+    BrandListComponent, 
+    CategoryListComponent, 
+    ImageListComponent
+  ]
+})
+export class MaintenanceModule { }
